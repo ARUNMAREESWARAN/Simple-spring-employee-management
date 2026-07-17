b8b16c58728f4c26e697f9ce166d63e769a14b77
ARUN MAREESWARAN <147586129+ARUNMAREESWARAN@users.noreply.github.com>
2026-07-17 09:46:48 +0530

    Create dto/EmployeeDto.java

---
+++ dto/EmployeeDto.java
@@ -0,0 +1,14 @@
+package com.employeemanagement.project.dto;
+
+import lombok.Data;
+
+@Data
+public class DepartmentDto {
+
+    private int id;
+
+    private String name;
+
+    private String location;
+
+}
