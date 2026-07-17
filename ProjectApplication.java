8eec26f3d1379643f96c14e787f719a80f3eea05
ARUN MAREESWARAN <147586129+ARUNMAREESWARAN@users.noreply.github.com>
2026-07-17 09:55:46 +0530

    Create ProjectApplication.java

---
+++ ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+}
