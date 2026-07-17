9abd4a26b82c8a4baa6d682ba24aace0ffa1a5b1
ARUN MAREESWARAN <147586129+ARUNMAREESWARAN@users.noreply.github.com>
2026-07-17 09:47:38 +0530

    Update EmployeeDto.java

--- dto/EmployeeDto.java
+++ dto/EmployeeDto.java
@@ -2,13 +2,23 @@ package com.employeemanagement.project.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
-public class DepartmentDto {
+public class EmployeeDto {
 
     private int id;
 
     private String name;
 
-    private String location;
+    private String email;
+
+    private double salary;
+
+    private LocalDate joiningDate;
+
+    private boolean isActive;
+
+    private DepartmentDto department;
 
 }
